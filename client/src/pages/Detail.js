f1e9589f729b935350b219b873cf227a42cd1134
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-07-18 14:08:23 -0400

    added functionality to update and delete cart items

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -48,6 +48,13 @@ function Detail() {
     }
   };
 
+  const removeFromCart = () => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
+    })
+  }
+
   return (
     <>
       {currentProduct ? (
@@ -61,7 +68,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
           </p>
 
           <img
